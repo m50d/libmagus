3279e54b175cc77a3adef21bde4a13b1325ffb83
Michael Donaghy <md401@srcf.ucam.org>
2012-01-03 19:41:50 +0000

    Stuffs to have a main

--- src/main/java/net/homelinux/md401/magus/FileHandler.java
+++ src/main/java/net/homelinux/md401/magus/FileHandler.java
@@ -11,10 +11,10 @@ import net.homelinux.md401.magus.ed2k.JacksumWrapper;
 public class FileHandler {
 	private final ConnectionWrapper connectionWrapper = new ConnectionWrapper();
 
-	public void addFile(final String username, final String password, final boolean watched, final int length, final File data) {
+	public void addFile(final String username, final String password, final boolean watched, final File data) {
 		try {
-			final String hash = JacksumWrapper.ed2k(data.getPath());
-			connectionWrapper.add(username, password, length, hash, watched);
+			final String hash = JacksumWrapper.ed2k(data.getAbsolutePath());
+			connectionWrapper.add(username, password, (int) data.length(), hash, watched);
 		} catch (final IOException e) {
 			throw new RuntimeException(e);
 		} catch (final UdpConnectionException e) {
@@ -22,5 +22,9 @@ public class FileHandler {
 		} catch (final AniDbException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public static void main(String args[]) {
+		new FileHandler().addFile(args[0], args[1], true, new File(args[2]));
 	}
 }
